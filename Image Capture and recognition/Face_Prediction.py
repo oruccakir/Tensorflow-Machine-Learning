cd96356b60ff2477d8c9be9c865ad32874532e1c
oruccakir <oruccakir2525@gmail.com>
2023-09-22 08:33:10 +0300

    Face Recognition is completed

---
+++ Image Capture and recognition/Face_Prediction.py
@@ -0,0 +1,32 @@
+from FaceModel import FaceModel
+import matplotlib.pyplot as plt
+import cv2
+
+path = "C:\\Users\\orucc\\Desktop\\oruc_and_erdem"
+
+face_model= FaceModel(path)
+
+
+while True:
+
+    face_model.say_the_result("Show me yourself by looking at the camera?")
+
+    command = input("Enter command : ")
+
+    if command == "1":
+        image = face_model.capture_the_image()
+        
+        pred_class = face_model.pred_and_plot(filename="Image Capture and recognition\\Captured_images\\image.jpg")
+
+        text = f"Person is detected Estimate  {str(pred_class)}"
+
+        print("Estimate : ",str(pred_class))
+
+        face_model.say_the_result(text=text)
+
+        cv2.imshow("Me",image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    else:
+       break
